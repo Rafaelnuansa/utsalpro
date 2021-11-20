3ffc8436e565b68a0008ca27a7d3d520023f45a7
Rafael Nuansa <iandrewnew@gmail.com>
2021-11-20 09:21:06 +0700

    Initial commit

---
+++ src/utsalpro/Variabel.java
@@ -0,0 +1,15 @@
+package utsalpro;
+
+public class Variabel {
+ public static void main(String args[]) {
+	 String salam, nama; 
+	 int tahun; 
+	 salam = "Assalamualaikum";
+	 nama = "Rafael Nuansa Ramadhon";
+	 tahun = 2000;
+	 
+	 System.out.println(salam);
+	 System.out.println("Nama Saya : "+nama);
+	 System.out.println("Saya Lahir di Tahun : "+tahun);
+ }
+}
